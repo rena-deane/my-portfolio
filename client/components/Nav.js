c0cca19382b39f1eec75cb27f0ad1ffdd498e2aa
Rena Goldsmith <rena.kg@gmail.com>
2016-08-17 13:34:32 +1200

    add reset css to remove gap at top of page

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -4,13 +4,21 @@ class Nav extends Component {
   render () {
     return (
       <div id='nav-wrapper'>
-        <nav className='navbar navbar-default'>
-          <ul className='nav navbar-nav'>
+        <nav className='navbar'>
+          <ul className='navlist twelve columns'>
             <span className='home-logo'><a href='#'>Logo</a></span>
-            <li><a href='#about'>About</a></li>
-            <li><a href='#projects'>Projects</a></li>
-            <li><a href='#work'>Work</a></li>
-            <li><a href='#connect'>Connect</a></li>
+            <li>
+              <a href='#about'>About</a>
+            </li>
+            <li>
+              <a href='#projects'>Projects</a>
+            </li>
+            <li>
+              <a href='#work'>Work</a>
+            </li>
+            <li>
+              <a href='#connect'>Connect</a>
+            </li>
           </ul>
         </nav>
       </div>
