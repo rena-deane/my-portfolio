6bd329b644857d433650241303de68d71d64c54f
Rena Goldsmith <rena.kg@gmail.com>
2016-08-17 17:40:54 +1200

    add react bootstrap to create navbar

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import { Navbar, Nav, NavItem, MenuItem, Header } from 'react-bootstrap'
+import { Navbar, Nav, NavItem } from 'react-bootstrap'
 
 class Navigation extends Component {
 
   render () {
     return (
-      <Navbar inverse>
+      <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
             <a href='#'>React-Bootstrap</a>
@@ -13,14 +13,6 @@ class Navigation extends Component {
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
-          <Nav>
-            <NavItem eventKey={1} href='#'>
-              Link
-            </NavItem>
-            <NavItem eventKey={2} href='#'>
-              Link
-            </NavItem>
-          </Nav>
           <Nav pullRight>
             <NavItem eventKey={1} href='#'>
               Link Right
