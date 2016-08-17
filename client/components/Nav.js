f7284fe6bcc02b2e0d11cc176d19a57291807274
Rena Goldsmith <rena.kg@gmail.com>
2016-08-17 16:32:02 +1200

    play around with react-bootstrap

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react'
+import { Navbar, Nav, NavItem, MenuItem, Header } from 'react-bootstrap'
 
-class Nav extends Component {
+class Navigation extends Component {
+
   render () {
     return (
-      <div id='nav-wrapper'>
-        <nav className='navbar'>
-          <ul className='navlist twelve columns'>
-            <span className='home-logo'><a href='#'>Logo</a></span>
-            <li>
-              <a href='#about'>About</a>
-            </li>
-            <li>
-              <a href='#projects'>Projects</a>
-            </li>
-            <li>
-              <a href='#work'>Work</a>
-            </li>
-            <li>
-              <a href='#connect'>Connect</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <Navbar inverse>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href='#'>React-Bootstrap</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href='#'>
+              Link
+            </NavItem>
+            <NavItem eventKey={2} href='#'>
+              Link
+            </NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href='#'>
+              Link Right
+            </NavItem>
+            <NavItem eventKey={2} href='#'>
+              Link Right
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     )
   }
 }
 
-export default Nav
+export default Navigation
