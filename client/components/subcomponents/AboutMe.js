012983d0df17044b9c969a2fab343dd9eafd053f
Rena Goldsmith <rena.kg@gmail.com>
2016-08-29 16:32:57 +1200

    add modals to about

---
+++ client/components/subcomponents/AboutMe.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import { Button, Modal, ModalHeader, OverlayTrigger, Popover, Tooltip } from 'react-bootstrap'
+
+
+class AboutMe extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      showModal: false,
+      isFetching: true
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ isFetching: false })
+  }
+
+  close() {
+    this.setState({ showModal: false })
+  }
+
+  open() {
+    this.setState({ showModal: true })
+  }
+
+  render() {
+    const popover = (
+      <Popover id="modal-popover" title="popover">
+        very popover. such engagement
+      </Popover>
+    )
+    const tooltip = (
+      <Tooltip id="modal-tooltip">
+        wow.
+      </Tooltip>
+    )
+
+    return (
+      <div>
+        <Button
+          bsStyle="primary"
+          bsSize="large"
+          onClick={this.open.bind(this)}
+        >
+          Ko Au / About Me
+        </Button>
+
+        <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+          <Modal.Header closeButton>
+            <Modal.Title className='about-headings'>Ko Au / About Me</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p className='about-content'>
+              Born and bred on the East Coast of the North Island, Ruatoria and Rangitukia.
+            </p>
+
+            <p className='about-content'>
+              I was raised and immersed in a culturally strong environment where the traditions, practices and perspectives of my whanau, hapu and iwi were exercised daily.
+            </p>
+
+            <p className='about-content'>
+              This cultural strength provides me with the confidence and competence to act, think and behave in ways that allow me to recognise and respect the distinctiveness of others.
+            </p>
+
+            <h6>Coming soon ... map of the East Coast</h6>
+          </Modal.Body>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+export default AboutMe
---
+++ client/components/subcomponents/TechMe.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import { Button, Modal, ModalHeader, OverlayTrigger, Popover, Tooltip } from 'react-bootstrap'
+
+
+class AboutMe extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      showModal: false,
+      isFetching: true
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ isFetching: false })
+  }
+
+  close() {
+    this.setState({ showModal: false })
+  }
+
+  open() {
+    this.setState({ showModal: true })
+  }
+
+  render() {
+    const popover = (
+      <Popover id="modal-popover" title="popover">
+        very popover. such engagement
+      </Popover>
+    )
+    const tooltip = (
+      <Tooltip id="modal-tooltip">
+        wow.
+      </Tooltip>
+    )
+
+    return (
+      <div>
+        <Button
+          bsStyle="primary"
+          bsSize="large"
+          onClick={this.open.bind(this)}
+        >
+          Hangarau / Technology
+        </Button>
+
+        <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+          <Modal.Header closeButton>
+            <Modal.Title className='about-headings'>Hangarau / Technology</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p className='about-content'>
+              I challenged myself to follow that passion and pursue a career in Web Development.  As a result, I am a highly motivated and enthusiastic graduate of Enspiral Dev Academy where I continue to learn everyday.  I am motivated by the opportunities technology can provide to our people. Especially those in rural areas.  Small towns can benefit from untilising various technologies such as mapping, e-tourism, e-commerce and accessible learning resources.
+            </p>
+            <p className='about-content'>
+            I believe in the saying, “When you learn, teach“.  When I feel like I’ve learnt all I can I hope one day I’ll be able to introduce and teach programming to rural communities around the country starting with my beloved East Coast.
+            </p>
+          </Modal.Body>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+export default AboutMe
