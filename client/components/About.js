437f78fcaf2ecdabfe478f340837c85a057a315a
Rena Goldsmith <rena.kg@gmail.com>
2016-08-29 18:27:11 +1200

    create Subnav component

--- client/components/About.js
+++ client/components/About.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react'
 import AboutMe from './subcomponents/AboutMe'
 import TechMe from './subcomponents/TechMe'
 import Journey from './subcomponents/Journey'
+import SubNav from './subcomponents/SubNav'
 
 class About extends Component {
   render () {
     return (
       <div className='about-wrapper'>
         <div className='about origin col-xs-12 col-lg-6'>
-
+          <SubNav />
         </div>
 
         <div className='about journey col-xs-12 col-lg-6'>
 
-            <AboutMe />
-            <Journey />
-            <TechMe />
+          <AboutMe />
+          <Journey />
+          <TechMe />
 
         </div>
       </div>
