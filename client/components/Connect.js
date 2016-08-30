8db6a6ed893993e64434a4ce6be56913cb8d7bbd
Rena Goldsmith <rena.kg@gmail.com>
2016-08-30 13:18:59 +1200

    add icons to Connect and style

--- client/components/Connect.js
+++ client/components/Connect.js
@@ -3,8 +3,19 @@ import React, { Component } from 'react'
 class Connect extends Component {
   render () {
     return (
-      <div className='connect-wrapper'>
-        <h2>Connect information goes here</h2>
+        <div className='connect-wrapper'>
+          <h3 className='current-page'>Connect</h3>
+
+          <div className='quote-wrapper'>
+            <h1 className='quote maori'>“Kanohi ki te kanohi” </h1>
+          </div>
+
+          <ul className='connect-links'>
+            <li><img src='../../public/images/github.svg' className='social-icons'/></li>
+            <li><img src='../../public/images/linkedin.svg' className='social-icons'/></li>
+            <li><img src='../../public/images/twitter.svg' className='social-icons'/></li>
+          </ul>
+
       </div>
     )
   }
